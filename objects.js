421a1676c94f267d490ecd93f23d431d61f6a9de
helpful-database-6850 <antwaun.l.johnson@gmail.com>
2019-05-30 19:57:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,6 @@
 var playlist = {artist: 'song' };
 
-function updatePlaylist(playlist, artistName, songTitle)
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign(playlist{}, { artistName: songTitle});
+  return playlist;
+}
